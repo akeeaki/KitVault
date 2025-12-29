7eb0a46c3612bab1d36311d3cc3ba67e19e68e99
don1x <mityafotografer@gmail.com>
2025-12-29 15:34:59 +0300

    a

--- src/main/java/net/rnsqd/kitVault/schedulers/cooldown/CooldownSchedulerInstance.java
+++ src/main/java/net/rnsqd/kitVault/schedulers/cooldown/CooldownSchedulerInstance.java
@@ -3,6 +3,8 @@ package net.rnsqd.kitVault.schedulers.cooldown;
 import net.rnsqd.kitVault.KitVault;
 import org.bukkit.scheduler.BukkitScheduler;
 
+import java.util.HashMap;
+
 public final class CooldownSchedulerInstance implements Runnable {
     private final KitVault plugin;
 
@@ -13,6 +15,16 @@ public final class CooldownSchedulerInstance implements Runnable {
 
     @Override
     public void run() {
-        this.plugin
+        this.plugin.getDatabase().getPlayerRecords().forEach(p -> {
+            if (p.kitsCooldowns() != null) {
+                p.kitsCooldowns().forEach((key, value) -> {
+                    final long newCooldown = value - 1;
+                    if (newCooldown <= 0)
+                        p.kitsCooldowns().remove(key);
+                    else
+                        p.kitsCooldowns().put(key, newCooldown);
+                });
+            }
+        });
     }
 }
